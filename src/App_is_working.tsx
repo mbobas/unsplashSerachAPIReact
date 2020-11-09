c3261873d13c3524fa8dc3d3ee3895c279fdcbdb
Michal <mbobas@gmail.com>
2020-11-09 22:55:17 +0100

    unsplash lib instead axios used

--- src/App_is_working.tsx
+++ src/App_is_working.tsx
@@ -1,43 +1,50 @@
+  
 import React, {useState } from 'react';
 import ReactDOM from "react-dom";
 import axios from 'axios';
-import Unsplash, { toJson } from 'unsplash-js';
+//import Unsplash from 'unsplash-js';
 import env from './env.json'
 import "./App.css";
 
+// require syntax
+// const Unsplash = require('unsplash-js').default;
+// const toJson = require('unsplash-js').toJson;
+
+
+// const unsplash = new Unsplash({ accessKey: env.API_KEY });
 
 export default function App() { 
-    const unsplash = new Unsplash({ accessKey: env.API_KEY });
-
     const [photo, setPhoto] = useState("");
-    const [clientId, setClientId] = useState(env.API_KEY);
+    const [clientId, setClientId] = useState("Fh_M0A0Ou9gGMNFk41QFpiX_lY9VjwXH6K7RFidx0MA");
+    
     const [result, setResult] = useState([]);
 
     const handleChange = (event: any ) => {
         setPhoto(event.target.value);
-        console.log("handleChange: value");
     }
+    const handleSubmit = (event: any) => {
+        console.log(photo);
+        const url = 
+        "https://api.unsplash.com/search/photos?page=1&query="+
+        photo+
+        "&client_id="+
+        clientId;
 
-    const handleSearchPhotos = (event:any) => {
-        console.log("handleSearchPhotos: " + photo)
-        unsplash.search.photos(photo, 1, 10)
-            .then(toJson)
-            .then(json => {
-                console.log(json);
-                setResult(json.results)
-            });
+        axios.get(url).then(response => {
+            console.log(response.data.results);
+            setResult(response.data.results)
+        });
     }
-
     const onKeyDown = (event:any ) => {
         if (event.key === 'Enter') {
-            handleSearchPhotos(event);
+            handleSubmit(event);
           }
     }
     const autoCompleete = (event: any) => {
         if (event.target.value.length > 3)
         console.log(event.target.value.length + "StartAutocomplete")
     }
-
+     
     return (
         <div className="App"> 
             <div className="logo-and-searchbar-container">
@@ -53,13 +60,13 @@ export default function App() {
                         placeholder="Search for high resolution photos" 
                     />
                     <button 
-                        onClick={handleSearchPhotos} 
+                        onClick={handleSubmit} 
                         type="submit">
                         Search
                     </button>
                 </div>
             </div>
-            
+
             <div>
             {result.map((item: any) => (
                 <img src={item.urls.small} />
